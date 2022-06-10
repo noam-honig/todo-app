4782ac7ce488929e07bfe45b3919444a8ad33b75
Noam Honig <noam.honig@gmail.com>
2022-06-10 08:55:12 +0300

    Added mvc

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,104 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import './App.css';
+import { useState } from "react";
+import { Task } from "./shared/Task";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [tasks, setTasks] = useState<Task[]>([
+    { id: 'a', title: "Task a", completed: false },
+    { id: 'b', title: "Task b", completed: false },
+    { id: 'c', title: "Task c", completed: true },
+    { id: 'd', title: "Task d", completed: false },
+    { id: 'e', title: "Task e", completed: true }
+  ]);
+  const [newTaskTitle, setNewTaskTitle] = useState('');
+  const [editingTask, setEditingTask] = useState<Task>();
+  const [hideCompleted, setHideCompleted] = useState<boolean>(false);
+
+  const createNewTask = async () => {
+    if (newTaskTitle) {
+      setTasks([...tasks, { title: newTaskTitle, completed: false, id: tasks.length.toString() }]);
+      setNewTaskTitle('');
+    }
+  }
+
+  const setAll = async (completed: boolean) => {
+    setTasks(tasks.map(task => ({ ...task, completed })));
+  }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
-}
+    <>
+      <section className="todoapp">
+        <header className="header">
+          <h1>todos</h1>
+          <input className="new-todo"
+            placeholder="What needs to be done?"
+            autoFocus
+            value={newTaskTitle}
+            onChange={e => setNewTaskTitle(e.target.value)}
+            onBlur={createNewTask}
+            onKeyDown={e => { if (e.key === 'Enter') createNewTask() }} />
+        </header>
 
-export default App
+        <section className="main">
+          <input id="toggle-all"
+            className="toggle-all"
+            type="checkbox"
+            checked={tasks.length > 0 && tasks[0].completed}
+            onChange={e => setAll(e.target.checked)}
+          />
+          <label htmlFor="toggle-all">Mark all as complete</label>
+          <ul className="todo-list">
+            {tasks.filter(task => !hideCompleted || !task.completed)
+              .map(task => {
+
+                const titleChange = (title: string) => {
+                  setEditingTask({ ...editingTask!, title });
+
+                };
+                const saveTask = async () => {
+                  setTasks(tasks.map(t => t === task ? editingTask! : t));
+                  setEditingTask(undefined);
+                };
+
+                const setCompleted = async (completed: boolean) => {
+                  setTasks(tasks.map(t => t === task ? { ...task, completed } : t));
+                }
+
+
+                const deleteTask = async () => {
+                  setTasks(tasks.filter(t => t !== task));
+                };
+
+                return <li key={task.id} className={task.id === editingTask?.id ? 'editing' : task.completed ? 'completed' : ''}>
+                  <div className="view">
+                    <input className="toggle" type="checkbox"
+                      checked={task.completed}
+                      onChange={e => setCompleted(e.target.checked)}
+                    />
+                    <label  onClick={() => setEditingTask(task)}>{task.title}</label>
+                    <button className="destroy" onClick={deleteTask}></button>
+                  </div>
+                  <input className="edit"
+                    value={editingTask?.title || ''}
+                    onBlur={saveTask}
+                    onChange={e => titleChange(e.target.value)} />
+                </li>
+              })}
+          </ul>
+        </section>
+        <footer className="footer">
+          <button onClick={() => setHideCompleted(false)} >All </button>
+          <button onClick={() => setHideCompleted(true)}>Active</button>
+        </footer>
+      </section>
+
+
+      <footer className="info">
+        <p>Double-click to edit a todo</p>
+        <p>Created by <a href="http://www.github.com/remult/remult">remult</a></p>
+        <p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
+      </footer>
+    </>
+  );
+}
+export default App;
