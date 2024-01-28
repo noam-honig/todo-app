08bfb2ab216cbd34fb95230898a915282d8b924c
Noam Honig <noam.honig@gmail.com>
2024-01-28 18:45:26 +0200

    Updated Sign In

--- src/frontend/Auth.tsx
+++ src/frontend/Auth.tsx
@@ -3,19 +3,17 @@ import { remult, UserInfo } from 'remult'
 import App from './App'
 
 export default function Auth() {
-  const [signInUsername, setSignInUsername] = useState('')
   const [currentUser, setCurrentUser] = useState<UserInfo>()
   remult.user = currentUser
 
-  async function signIn(f: FormEvent) {
+  async function signIn(f: FormEvent<HTMLFormElement>) {
     f.preventDefault()
     const result = await fetch('/api/signIn', {
       method: 'POST',
-      body: JSON.stringify({ username: signInUsername }),
+      body: JSON.stringify(Object.fromEntries(new FormData(f.currentTarget))),
     })
     if (result.ok) {
       setCurrentUser(await result.json())
-      setSignInUsername('')
     } else alert(await result.json())
   }
   async function signOut() {
@@ -37,14 +35,10 @@ export default function Auth() {
       <>
         <main className="sign-in">
           <h2>Sign In</h2>
-          <form>
+          <form onSubmit={signIn}>
             <label>Name</label>
-            <input
-              value={signInUsername}
-              onChange={(e) => setSignInUsername(e.target.value)}
-              placeholder="Try Steve or Jane"
-            />
-            <button onClick={signIn}>Sign in</button>
+            <input name="username" placeholder="Try Steve or Jane" />
+            <button>Sign in</button>
           </form>
         </main>
       </>
